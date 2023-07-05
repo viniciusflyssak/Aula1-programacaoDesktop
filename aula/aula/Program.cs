cd85ed2b55cb00a0b8e5846006bed410db8dfaa2
Vinicius Flyssak <viniciusflyssak@gmail.com>
2023-07-05 01:38:04 -0300

    Adicionado arquivos

---
+++ aula/aula/Program.cs
@@ -0,0 +1,24 @@
+﻿import System
+import System.Console
+
+
+Dim x As Integer, y As Integer
+        On Error GoTo ErrorHandler
+        Write("Digite o valor 1: ")
+        x = ReadLine()
+        Write("Digite o valor 2: ")
+        y = ReadLine()
+
+        Dim c As Double 
+        c = x/y
+        WriteLine("O Resultado é " + c)
+        Exit Sub
+Errorhandler: 
+        WriteLine()
+        WriteLine("Dados incorretos. (A)bortar, (T)entar novamente, (I)gnorar")
+        Dim r As String
+        r = ReadLine()
+        Select Case r
+            Case "A": Exit Sub
+            Case "T": Resume
+            Case "I": Resum
